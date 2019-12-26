e7dcd00e17b266b88f88a3ab87e3201a521f5a3b
alexbertis <alexberti02@gmail.com>
2019-12-26 02:03:03 +0100

    Create React.JS Tic Tac Toe (GCI19-JBoss)
    Create a simple game, folllowing the official React tutorial with all the changes specified in the GCI 2019 task from JBoss Community.

---
+++ jboss-react-game/src/index.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            aIsNext: false,                 // Reverse starting order
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.aIsNext ? 'A' : 'Y';    // Player 'X' is player 'A', 'O' is 'Y'
+        this.setState({
+            squares: squares, 
+            aIsNext: !this.state.aIsNext
+        });
+    }
+
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    render() {
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Champion: ' + winner;     // Custom text at the end
+        } else {
+            status = 'Next player: ' + (this.state.aIsNext ? 'alexberti02 (A)' : 'randomPlayer7928 (Y)');   // Custom player names
+        }
+
+        return (
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            </div>
+        </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+        <div className="game">
+            <div className="game-board">
+            <Board />
+            </div>
+            <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+            </div>
+        </div>
+        );
+    }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+        [0, 4, 6],  // Custom win condition
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
